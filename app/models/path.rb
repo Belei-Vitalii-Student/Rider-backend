1c358e3b53691843e9e71f1d7d79247c21537416
vitPrograms <vitalii.beley@gmail.com>
2023-11-09 18:28:26 +0200

    disable validates

--- app/models/path.rb
+++ app/models/path.rb
@@ -6,17 +6,17 @@ class Path
 
   belongs_to :user, required: true
 
-  validates_each :coordinates do |record, attr, value|
-    if !value.present? || !value.kind_of?(Array)
-      record.errors.add(attr, "must contain only lat and lng value")
-    end
+  # validates_each :coordinates do |record, attr, value|
+  #   if !value.present? || !value.kind_of?(Array)
+  #     record.errors.add(attr, "must contain only lat and lng value")
+  #   end
 
 
-    value.each do|obj|
-      if !obj[:lat].present? || !obj[:lng].present?
-        record.errors.add(attr, "must contain only lat and lng value")
-      end
-    end
-  end
+  #   value.each do|obj|
+  #     if !obj[:lat].present? || !obj[:lng].present?
+  #       record.errors.add(attr, "must contain only lat and lng value")
+  #     end
+  #   end
+  # end
 
 end
